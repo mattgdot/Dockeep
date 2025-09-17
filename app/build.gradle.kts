46647d71390b49a939266ec562a1ff32e642c0aa
Matthew G. <matei.grejdan@gmail.com>
2025-09-17 10:49:22 +0300

    dependenciesInfo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.8-beta"
+        versionName = "0.9-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
