f7688f019c14e93c3ec0871c4d56d6821fd57707
Matthew G. <matei.grejdan@gmail.com>
2025-09-26 16:34:37 +0300

    Archive files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.app.dockeep"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1-beta"
+        versionCode = 4
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -86,6 +86,6 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     ksp(libs.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.biometric)
+   // implementation(libs.biometric)
     implementation(libs.foundation.pager)
 }
